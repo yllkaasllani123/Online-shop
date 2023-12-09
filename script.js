54118b0063f7da020219dcc805dd0b96def78781
yllkaasllani123 <ya68285@ubt-uni.net>
2023-12-09 21:56:28 +0100

    ndryshime

---
+++ script.js
@@ -0,0 +1,32 @@
+const switchers = [...document.querySelectorAll('.switcher')]
+
+switchers.forEach(item => {
+	item.addEventListener('click', function() {
+		switchers.forEach(item => item.parentElement.classList.remove('is-active'))
+		this.parentElement.classList.add('is-active')
+	})
+})
+
+let nameRegex = /^[A-Z][a-z]{3,8}$/;
+let emailRegex = /[a-zA-Z.-_]+@+[a-z]+\.+[a-z]{2,3}$/;
+
+    function validateForm(){
+        let nameInput = document.getElementById('name');
+        let nameError = document.getElementById('nameError');
+        let emailInput = document.getElementById('email');
+        let emailError = document.getElementById('emailError');
+
+        nameError.innerText = '';
+        emailError.innerText = '';
+
+        if(!nameRegex.test(nameInput.value)){
+            nameError.innerText = 'invalid name';
+            return;
+        }
+        if(!emailRegex.test(emailInput.value)){
+            emailError.innerText = 'invalid email';
+            return;
+        }
+
+        alert('form submitted succesfully!');
+    }
