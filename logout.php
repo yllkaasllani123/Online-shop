350be73ee12a466dd49e18f322a37401184638bf
yllkaasllani123 <ya68285@ubt-uni.net>
2024-02-01 17:03:03 +0100

    ndryshime

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
-// 
-session_start();
-session_destroy();
-header("Location: onlineshop.php"); 
-exit();
-?>
+
+logout.php :
+  session_start(); 
+  session_destroy();
+  header('Location: login.php');
+  ?>
